6c2a2018fa68bc4f4932cc741eb91f61fe663246
Ben Shutt <ben@3sidedcube.com>
2020-12-07 11:38:56 +0000

    Add Strava API as target

--- StravaAPI/main.swift
+++ StravaAPI/main.swift
@@ -1,15 +1,22 @@
+//
+//  main.swift
+//  StravaAPI
+//
+//  Created by Ben Shutt on 07/12/2020.
+//
+
 import Alamofire
 import Foundation
 import HTTPRequest
 
 do {
     HTTPRequest.Configuration.shared.responseLogging = true
-    
+
     try StravaSession.shared.configure()
-    
+
     let athlete: Athlete =
         try AF.requestSync(StravaAPI.athlete).successOrThrow()
-    
+
     debugPrint(athlete)
 
 } catch {
