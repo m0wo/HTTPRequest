6ae29010a6e0b3b402be0b081bb42fe9067db54c
benshutt <ben@3sidedcube.com>
2020-12-28 15:33:30 +0000

    Create Swagger Codegen generate script

---
+++ generate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# generate.sh
+# Use Swagger Codegen to generate Swift models from a Swagger spec.
+
+# Name of the script
+SCRIPT_NAME=$(basename -- "$0")
+
+# Repository name of the Swagger Codegen project
+SWAGGER_CODEGEN_REPO_NAME="Swagger Codegen"
+
+# Repository URL of the Swagger Codegen project
+SWAGGER_CODEGEN_REPO_URL="https://github.com/swagger-api/swagger-codegen"
+
+# Command of Swagger Codegen
+SWAGGER_CODEGEN_COMMAND="swagger-codegen"
+
+# Check Swagger Codegen is installed
+if ! [ -x "$(command -v ${SWAGGER_CODEGEN_COMMAND})" ]; then
+    # Require Swagger Codegen installation
+    echo "Please install ${SWAGGER_CODEGEN_REPO_NAME} from: ${SWAGGER_CODEGEN_REPO_URL}"
+    exit 1
+fi
+
+# Check a Swagger spec url is provided as argument
+if [[ $# -eq 0 ]]; then
+    echo "Usage: ./${SCRIPT_NAME} <SwaggerSpecURL>"
+    exit 1
+fi
+
+# Execute Swagger Codegen
+${SWAGGER_CODEGEN_COMMAND} generate -i $1 -l swift5
